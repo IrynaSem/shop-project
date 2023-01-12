05275faf3bda0e5d6625b29e0ad1844ac0873fd4
Iryna Semivolos <sio210389@gmail.com>
2023-01-12 16:19:59 +0200

    add app.js and function clicked

---
+++ app.js
@@ -0,0 +1,11 @@
+let productsCountEl = document.getElementById("products-count");
+console.log(productsCountEl);
+
+let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+console.log(addToCartBtns);
+
+for (let i = 0; i < addToCartBtns.length; i++) {
+  addToCartBtns[i].addEventListener("click", function () {
+    console.log("clicked");
+  });
+}
